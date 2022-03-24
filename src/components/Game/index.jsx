9b23e71564fc61f718d439e30b071d60b3f77e27
Albert Calasanz <acalasanzs@gmail.com>
2022-03-24 09:56:24 +0100

    Sphere

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, Component} from 'react'
 import { createRef } from 'react/cjs/react.production.min';
 import * as THREE from 'three';
+import * as GSAP from 'gsap';
 
 let scene, camera, renderer, cube;
 let game = createRef();
@@ -11,52 +12,58 @@ class Game extends Component {
     this.animate = this.animate.bind(this)
   }
 
-init () {
-//creating scene
-scene = new THREE.Scene();
-scene.background = new THREE.Color(0x2a3b4c);
+  init () {
+    //creating scene
+    scene = new THREE.Scene();
 
-//add camera
-camera = new THREE.PerspectiveCamera(
-    75,
-    game.current.clientWidth/game.current.clientHeight
-);
+    //add camera
+    camera = new THREE.PerspectiveCamera(
+        75,
+        game.current.clientWidth/game.current.clientHeight,
+        0.1,
+        1000
+    );
+    camera.position.z = 5;
 
-//renderer
-renderer = new THREE.WebGLRenderer();
-renderer.setSize(game.current.clientWidth, game.current.clientHeight);
+    //renderer
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setClearColor("#a29bfe")
+    renderer.setSize(game.current.clientWidth, game.current.clientHeight);
 
-//add geometry
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
-cube = new THREE.Mesh(geometry, material);
-
-scene.add(cube);
-
-camera.position.z = 5;
+    window.addEventListener('resize', () => {
+      renderer.setSize(game.current.clientWidth, game.current.clientHeight);
+      camera.aspect = game.current.clientWidth/game.current.clientHeight;
+      camera.updateProjectionMatrix()
+    })
 
 
-return renderer.domElement;
+    var geometry = new THREE.SphereGeometry(1, 10, 10);
+    var material = new THREE.MeshLambertMaterial({color: 0xFFCC00})
+    var mesh = new THREE.Mesh(geometry, material);
 
-}
+    scene.add(mesh)
 
-//animation
-animate(){
-  requestAnimationFrame(this.animate);
+    var light = new THREE.PointLight(0xFFFFFF, 1, 500);
+    light.position.set(10,0,25);
+    scene.add(light)
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+    this.animate()
+    return renderer.domElement;
 
-  renderer.render(scene, camera);
-}
-componentDidMount() {
-  game.current.appendChild(this.init());
-  this.animate();
-}
+  }
 
-render() {    
-    return <div ref={game} className='content-container'></div>;
-}
+  //animation
+  animate(){
+    renderer.render(scene,camera)
+  }
+  componentDidMount() {
+    game.current.appendChild(this.init());
+    this.animate();
+  }
+
+  render() {    
+      return <div ref={game} className='content-container'></div>;
+  }
 }
 
 export default Game
