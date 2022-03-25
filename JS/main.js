f3065d2ed80fc645412c95668147e60d3c2dcae6
Albert Calasanz <acalasanzs@gmail.com>
2022-03-25 19:33:30 +0100

    lol

--- JS/main.js
+++ JS/main.js
@@ -28,149 +28,349 @@ document.addEventListener("DOMContentLoaded",e=>{
     });
 });
 const game = document.getElementById("game");
-let scene, camera, renderer, skyboxGeo, skybox, controls, myReq;
-let autoRotate = true;
 
-function createMaterialArray() {
-  const skyboxImagepaths = ["images/menu/front.jpg", "images/menu/back.jpg", "images/menu/up.jpg", "images/menu/down.jpg", "images/menu/right.jpg", "images/menu/left.jpg",];
-  const materialArray = skyboxImagepaths.map(image => {
-    let texture = new THREE.TextureLoader().load(image);
-
-    return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
-  });
-  return materialArray;
-}
-
-function init() {
-
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(
-    55,
-    game.clientWidth/ game.clientHeight,
-    45,
-    30000,
-  );
-  camera.position.set(5493, 3556, -4089);
-  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true,
-    logarithmicDepthBuffer: true });
-  renderer.setSize(game.clientWidth, game.clientHeight);
-  renderer.setClearColor(0x574b90, 1);
-  renderer.gammaOutput = true; 
-  game.appendChild(renderer.domElement);
-  const materialArray = createMaterialArray();
-
-  skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
-  skybox = new THREE.Mesh(skyboxGeo, materialArray);
-
-  var spriteMap = new THREE.TextureLoader().load( "images/menu/schedablocks.png" );
-  var spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
-  var sprite = new THREE.Sprite( spriteMaterial );
-  sprite.scale.set(8000, 8000, 8000)
-  scene.add( sprite );
-
-
-  //Luces y ya!
-  const upColor = 0xFFFF80;
-  const downColor = 0x4040FF;
-
-  var light = new THREE.HemisphereLight(upColor, downColor, 100);
-  var light2 = new THREE.DirectionalLight(0xFFFFFF, 10);
-  light2.position.set(100, -500, 100);
-  var light3 = new THREE.DirectionalLight(0x574b90, 10);
-  light3.position.set(100, 500, 100);
-  var ambient = new THREE.AmbientLight("#85b2cd", 100);
-  scene.add(light3);
-  scene.add(light2);
-  scene.add(light);
-  scene.add(ambient);
-
-  let instance = new THREE.TextSprite({
-    alignment: 'center',
-    color: '#000',
-    fontFamily: 'Arial, Helvetica, sans-serif',
-    fontSize: 1000,
-    fontStyle: 'italic',
-    text: "Loading...",
-  });
-  instance.name = "loading";
-  scene.add(instance);
-
-
-    var manager = new THREE.LoadingManager();
-    manager.onProgress = function ( item, loaded, total ) {};
-
-    // 3Dモデル用テクスチャ画像の読込
-    var loader = new THREE.GLTFLoader();
-
-    // Load a glTF resource
-    loader.load(
-        // resource URL
-        'scene.gltf',
-        // called when the resource is loaded
-        function ( gltf ) {
-
-                mesh = gltf.scene;
-                mesh.scale.set( 800, 800, 800 );
-
-                if (mesh){
-                  scene.remove( scene.getObjectByName(instance.name) );
-                  animate();
-                }
-                scene.add( mesh );
-    
-                //scene.add( gltf.scene );
-
-                //gltf.animations; // Array<THREE.AnimationClip>
-                //gltf.scene; // THREE.Scene
-                //gltf.scenes; // Array<THREE.Scene>
-                //gltf.cameras; // Array<THREE.Camera>
-                //gltf.asset; // Object
-
-        },
-        // called when loading is in progresses
-        function ( xhr ) {
-
-                console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-        },
-        // called when loading has errors
-        function ( error ) {
-
-                console.log( 'An error happened' );
-
-        }
+function main() {
+  let scene, camera, renderer, skyboxGeo, skybox, controls, myReq;
+  let menuText, menuWeapon;
+  let autoRotate = true;
+  
+  function createMaterialArray() {
+    const skyboxImagepaths = ["images/menu/front.jpg", "images/menu/back.jpg", "images/menu/up.jpg", "images/menu/down.jpg", "images/menu/right.jpg", "images/menu/left.jpg",];
+    const materialArray = skyboxImagepaths.map(image => {
+      let texture = new THREE.TextureLoader().load(image);
+  
+      return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide});
+    });
+    return materialArray;
+  }
+  
+  function init() {
+  
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+      55,
+      game.clientWidth/ game.clientHeight,
+      45,
+      30000,
     );
-
-
-
-
+    camera.position.set(5493, 3556, -4089);
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true,
+      logarithmicDepthBuffer: true });
+    renderer.setSize(game.clientWidth, game.clientHeight);
+    renderer.setClearColor(0x574b90, 1);
+    renderer.gammaOutput = true; 
+    if (document.querySelector("canvas")){
+      document.querySelector("canvas").remove()
+    }
+    game.appendChild(renderer.domElement);
+    const materialArray = createMaterialArray();
   
-
-  scene.add(skybox);
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.enabled = true;
-  controls.autoRotate = true;
-  controls.autoRotateSpeed = 1.0;
-  controls.minDistance = 700;
-  controls.maxDistance = 20000;
-  window.addEventListener('resize', onWindowResize, false);
-  animate();
+    skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
+    skybox = new THREE.Mesh(skyboxGeo, materialArray);
   
-}
-function onWindowResize() {
-  camera.aspect = game.clientWidth / game.clientHeight;
-
-  camera.updateProjectionMatrix();
-  renderer.setSize(game.clientWidth, game.clientHeight);
-}
-
-function animate() {
+    var spriteMap = new THREE.TextureLoader().load( "images/menu/schedablocks.png" );
+    var spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
+    menuText = new THREE.Sprite( spriteMaterial );
+    menuText.scale.set(8000, 8000, 8000)
+    scene.add( menuText );
+    menuText.name = "menuText";
+  
+  
+    let instance = new THREE.TextSprite({
+      alignment: 'center',
+      color: '#000',
+      fontFamily: 'Arial, Helvetica, sans-serif',
+      fontSize: 1000,
+      fontStyle: 'italic',
+      text: "Loading...",
+    });
+    instance.name = "loading";
+    scene.add(instance);
+  
+  
+    lights()
+  
+  
+      var manager = new THREE.LoadingManager();
+      manager.onProgress = function ( item, loaded, total ) {};
+  
+      // 3Dモデル用テクスチャ画像の読込
+      var loader = new THREE.GLTFLoader();
+  
+      // Load a glTF resource
+      loader.load(
+          // resource URL
+          'scene.gltf',
+          // called when the resource is loaded
+          function ( gltf ) {
+  
+                  mesh = gltf.scene;
+                  mesh.scale.set( 800, 800, 800 );
+  
+                  if (mesh){
+                    scene.remove( scene.getObjectByName(instance.name) );
+                    animate();
+                  }
+                  scene.add( mesh );
+      
+                  //scene.add( gltf.scene );
+  
+                  //gltf.animations; // Array<THREE.AnimationClip>
+                  //gltf.scene; // THREE.Scene
+                  //gltf.scenes; // Array<THREE.Scene>
+                  //gltf.cameras; // Array<THREE.Camera>
+                  //gltf.asset; // Object
+  
+          },
+          // called when loading is in progresses
+          function ( xhr ) {
+  
+                  console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+          },
+          // called when loading has errors
+          function ( error ) {
+  
+                  console.log( 'An error happened' );
+  
+          }
+      );
+  
+  
+  
+  
+    
+  
+    scene.add(skybox);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enabled = true;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 1.0;
+    controls.minDistance = 700;
+    controls.maxDistance = 20000;
+    window.addEventListener('resize', onWindowResize, false);
+    animate();
+    
+  }
+  function lights() {
+    //Luces y ya!
+    const upColor = 0xFFFF80;
+    const downColor = 0x4040FF;
+  
+    var light = new THREE.HemisphereLight(upColor, downColor, 100);
+    var light2 = new THREE.DirectionalLight(0xFFFFFF, 10);
+    light2.position.set(100, -500, 100);
+    var light3 = new THREE.DirectionalLight(0x574b90, 10);
+    light3.position.set(100, 500, 100);
+    var ambient = new THREE.AmbientLight("#85b2cd", 100);
+    scene.add(light3);
+    scene.add(light2);
+    scene.add(light);
+    scene.add(ambient);
+  }
+  function animate() {
     controls.autoRotate = autoRotate;
     
     controls.update();
+  
+    
     renderer.render(scene, camera);
     myReq = window.requestAnimationFrame(animate);
    
+  }
+  function onWindowResize() {
+    camera.aspect = game.clientWidth / game.clientHeight;
+  
+    camera.updateProjectionMatrix();
+    renderer.setSize(game.clientWidth, game.clientHeight);
+  }
+  init()
 }
 
-init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function scenario1() {
+  const renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(game.clientWidth, game.clientHeight);
+  const fov = 60;
+  const aspect = game.clientWidth/ game.clientHeight;  // the canvas default
+  const near = 0.1;
+  const far = 200;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 30;
+
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('white');
+  game.appendChild(renderer.domElement);
+
+  // put the camera on a pole (parent it to an object)
+  // so we can spin the pole to move the camera around the scene
+  const cameraPole = new THREE.Object3D();
+  scene.add(cameraPole);
+  cameraPole.add(camera);
+
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    camera.add(light);
+  }
+
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+  function rand(min, max) {
+    if (max === undefined) {
+      max = min;
+      min = 0;
+    }
+    return min + (max - min) * Math.random();
+  }
+
+  function randomColor() {
+    return `hsl(${rand(360) | 0}, ${rand(50, 100) | 0}%, 50%)`;
+  }
+
+  const numObjects = 100;
+  for (let i = 0; i < numObjects; ++i) {
+    const material = new THREE.MeshPhongMaterial({
+      color: randomColor(),
+    });
+
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    cube.position.set(rand(-20, 20), rand(-20, 20), rand(-20, 20));
+    cube.rotation.set(rand(Math.PI), rand(Math.PI), 0);
+    cube.scale.set(rand(3, 6), rand(3, 6), rand(3, 6));
+  }
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  class PickHelper {
+    constructor() {
+      this.raycaster = new THREE.Raycaster();
+      this.pickedObject = null;
+      this.pickedObjectSavedColor = 0;
+    }
+    pick(normalizedPosition, scene, camera, time) {
+      // restore the color if there is a picked object
+      if (this.pickedObject) {
+        this.pickedObject.material.emissive.setHex(this.pickedObjectSavedColor);
+        this.pickedObject = undefined;
+      }
+
+      // cast a ray through the frustum
+      this.raycaster.setFromCamera(normalizedPosition, camera);
+      // get the list of objects the ray intersected
+      const intersectedObjects = this.raycaster.intersectObjects(scene.children);
+      if (intersectedObjects.length) {
+        // pick the first object. It's the closest one
+        this.pickedObject = intersectedObjects[0].object;
+        // save its color
+        this.pickedObjectSavedColor = this.pickedObject.material.emissive.getHex();
+        // set its emissive color to flashing red/yellow
+        this.pickedObject.material.emissive.setHex((time * 8) % 2 > 1 ? 0xFFFF00 : 0xFF0000);
+      }
+    }
+  }
+
+  const pickPosition = {x: 0, y: 0};
+  const pickHelper = new PickHelper();
+  clearPickPosition();
+
+  function render(time) {
+    time *= 0.001;  // convert to seconds;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    cameraPole.rotation.y = time * .1;
+
+    pickHelper.pick(pickPosition, scene, camera, time);
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+
+  function getCanvasRelativePosition(event) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+      x: (event.clientX - rect.left) * canvas.width  / rect.width,
+      y: (event.clientY - rect.top ) * canvas.height / rect.height,
+    };
+  }
+
+  function setPickPosition(event) {
+    const pos = getCanvasRelativePosition(event);
+    pickPosition.x = (pos.x / canvas.width ) *  2 - 1;
+    pickPosition.y = (pos.y / canvas.height) * -2 + 1;  // note we flip Y
+  }
+
+  function clearPickPosition() {
+    // unlike the mouse which always has a position
+    // if the user stops touching the screen we want
+    // to stop picking. For now we just pick a value
+    // unlikely to pick something
+    pickPosition.x = -100000;
+    pickPosition.y = -100000;
+  }
+  window.addEventListener('mousemove', setPickPosition);
+  window.addEventListener('mouseout', clearPickPosition);
+  window.addEventListener('mouseleave', clearPickPosition);
+
+  window.addEventListener('touchstart', (event) => {
+    // prevent the window from scrolling
+    event.preventDefault();
+    setPickPosition(event.touches[0]);
+  }, {passive: false});
+
+  window.addEventListener('touchmove', (event) => {
+    setPickPosition(event.touches[0]);
+  });
+
+  window.addEventListener('touchend', clearPickPosition);
+}
+main();
+
+
+document.getElementById("new").addEventListener("click",_=>{if(document.querySelector("canvas"))document.querySelector("canvas").remove();scenario1()}, false);
+document.getElementById("schedablocks").addEventListener("click",_=>{if(document.querySelector("canvas"))document.querySelector("canvas").remove();main()}, false);
